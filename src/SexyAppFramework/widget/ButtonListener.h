23a043258f79a5d1868a76ff8b56a6cd48ebf829
Kyle Sylvestre <kyles3133@gmail.com>
2026-03-16 18:47:36 -0400

    add default implementation for listeners

--- src/SexyAppFramework/widget/ButtonListener.h
+++ src/SexyAppFramework/widget/ButtonListener.h
@@ -31,12 +31,12 @@ namespace Sexy
 class ButtonListener
 {
 public:
-	virtual void			ButtonPress(int theId) = 0;
-	virtual void			ButtonDepress(int theId) = 0;
-	virtual void			ButtonDownTick(int theId) = 0;
-	virtual void			ButtonMouseEnter(int theId) = 0;
-	virtual void			ButtonMouseLeave(int theId) = 0;
-	virtual void			ButtonMouseMove(int theId, int theX, int theY) = 0;
+	virtual void			ButtonPress(int theId) {}
+	virtual void			ButtonDepress(int theId) {}
+	virtual void			ButtonDownTick(int theId) {}
+	virtual void			ButtonMouseEnter(int theId) {}
+	virtual void			ButtonMouseLeave(int theId) {}
+	virtual void			ButtonMouseMove(int theId, int theX, int theY) {}
 };
 
 }
--- src/SexyAppFramework/widget/ListListener.h
+++ src/SexyAppFramework/widget/ListListener.h
@@ -31,9 +31,9 @@ namespace Sexy
 class ListListener 
 {
 public:
-	virtual void ListClicked(int theId, int theIdx, int theClickCount) = 0;
-	virtual void ListClosed(int theId) = 0;
-	virtual void ListHiliteChanged(int theId, int theOldIdx, int theNewIdx) = 0;
+	virtual void ListClicked(int theId, int theIdx, int theClickCount) {}
+	virtual void ListClosed(int theId) {}
+	virtual void ListHiliteChanged(int theId, int theOldIdx, int theNewIdx) {}
 };
 
 }
